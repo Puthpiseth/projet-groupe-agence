0b84c4a7a152808021fd3c82d0090c758995e7cb
puthpiseth <puthpiseth_tun@yahoo.com>
2021-02-01 16:53:45 +0100

    Update the social network links

--- main.js
+++ main.js
@@ -17,12 +17,31 @@ if (window.pageYOffset > 0) {
 
 
 
-let counter = 1;
+var slideIndex = 1;
+showSlides = slideIndex;
 
-setInterval(function(){
-    document.getElementById("radio" + counter).checked = true;
-    counter++;
-    if (counter > 4) {
-        counter = 1;
-    }
-}, 5000);
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
